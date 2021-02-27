80ce443da60a23c83e85563c0bc003bbe3adf8fd
karklis <claudiokarklis@gmail.com>
2021-02-27 14:47:28 -0300

    Initial commit

---
+++ moveit-next/src/components/ExperienceBar.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/ExperienceBar.module.css';
+
+export function ExperienceBar() {
+    const { currenteExperience, experienceToNextLevel } =useContext(ChallengesContext);
+
+    const percentToNextLevel = Math.round(currenteExperience * 100) / experienceToNextLevel;
+
+    return(
+        <header className={styles.experienceBar}>
+            <span>0 xp</span>
+            <div>
+                <div style={{ width:`${percentToNextLevel}%` }} />
+                <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
+                    {currenteExperience} xp
+                </span>
+            </div>
+            <span>{experienceToNextLevel} xp</span>
+        </header>
+    );
+}
